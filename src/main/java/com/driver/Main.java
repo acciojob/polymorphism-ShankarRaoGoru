cc84408db7aba5908a445960636b97b6dbc01113
shankargoru <durgagoru999@gmail.com>
2023-01-13 16:50:56 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.Product(5,6);
+        p.Product(5,6,7);
+        p.Product(5.5,7.8);
+
+    }
+
+}
+class Product{
+    public int Product(int x,int y){
+        return x*y;
+    }
+    public int Product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double Product(double x,double y){
+        return x*y;
+    }
 }
